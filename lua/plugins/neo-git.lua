e44e9373e6b9ffd5978cc872ba1adfc0e46176df
FranchoPlz <ferriolsfran@gmail.com>
2024-01-25 18:03:21 +0100

    Added keymaps for neogit

--- lua/plugins/neo-git.lua
+++ lua/plugins/neo-git.lua
@@ -7,8 +7,11 @@ return {
 
       -- Only one of these is needed, not both.
       "nvim-telescope/telescope.nvim", -- optional
-      "ibhagwan/fzf-lua",              -- optional
     },
-    config = true
+    config = function ()
+      local neogit = require("neogit")
+      neogit.setup()
+      vim.keymap.set('n', '<leader>gt', ':Neogit<CR>', {})
+    end
   }
 }
