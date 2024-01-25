4920e3c9808a5a429790446a0ba8a847ab4be756
FranchoPlz <ferriolsfran@gmail.com>
2024-01-25 12:55:21 +0100

    Added window and tab keymaps

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,3 +1,4 @@
+-- Default vim settings
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
@@ -5,7 +6,26 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set clipboard=unnamed")
 vim.g.mapleader = " "
 
-vim.keymap.set('n', '<F5>', ':below 10sp | terminal<CR>', {})
-vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]],{})
-vim.keymap.set({'t', 'n'}, '<A-j>', [[<C-\><C-n><C-w>j]], {})
-vim.keymap.set({'t', 'n'}, '<A-k>', [[<C-\><C-n><C-w>k]], {})
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true}
+
+-- Terminal window switching keymaps
+keymap.set('n', '<F5>', ':below 10sp | terminal<CR>', opts)
+keymap.set('t', '<Esc>', [[<C-\><C-n>]],opts)
+keymap.set({'t', 'n'}, '<A-j>', [[<C-\><C-n><C-w>j]], opts)
+keymap.set({'t', 'n'}, '<A-k>', [[<C-\><C-n><C-w>k]], opts)
+
+-- Tabs keymaps
+keymap.set('n', '<C-n>', ':tabedit<CR>', opts)
+keymap.set('n', '<tab>', ':tabnext<CR>', opts)
+keymap.set('n', '<s-tab>', ':tabprev<CR>', opts)
+
+-- New window
+keymap.set('n', 'ss', ':split<CR>', opts)
+keymap.set('n', 'sv', ':vsplit<CR>', opts)
+
+-- Move windows
+keymap.set('n', 'sh', '<C-w>h', opts)
+keymap.set('n', 'sk', '<C-w>k', opts)
+keymap.set('n', 'sj', '<C-w>j', opts)
+keymap.set('n', 'sl', '<C-w>l', opts)
